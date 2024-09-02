e34de6ce13114746b8776bbb136ee4a49dbb9c81
Van Whiteman <van.whiteman1@students.mq.edu.au>
2024-09-02 14:25:10 +1000

    initial commit owhaley

---
+++ R/owhaley.R
@@ -0,0 +1,54 @@
+#' owhaley
+#'
+#' @param what text string; Whatever phrase you want whale to echo
+#' @description
+#' summon a whale for customizable pick me up
+#'
+#' @return whale with phrase
+#' @export
+#'
+#' @examples say(what = "you're whale-come!")
+say <- function(what){
+  #Defining the whale ASCII
+  whale = "\n            ------ \n           %s \n            ------ \n               \\\   \n                \\\  \n                 \\\
+     .-'
+'--./ /     _.---.
+'-,  (__..-`       \\
+   \\          .     |
+    `,.__.   ,__.--/
+     '._/_.'___.-`
+"
+  #Finding the position of where to print message
+  what_pos_start <-
+    regexpr('%s', whale)[1] - 1
+
+  what_pos_end <- what_pos_start + 3
+
+  #If what isn't supplied...
+  if(missing(what)){
+    what <- phrases %>% sample(size = 1) #See here's the pipe!
+  }
+
+  #Combining positions, message and whale together
+  out <- paste0(substr(whale, 1, what_pos_start), #Top of speech bubble
+                what, #User specified message
+                substr(whale, what_pos_end, nchar(whale))) #Bottom of speech bubble + whale
+
+  #Return whale message
+  message(out)
+}
+
+
+#' Whale pun phrases used in say()
+#' @keywords internal
+phrases <-  c(
+  "You're whale-come!",
+  "How are you? I'm whaley good!",
+  "I'm having a whale of a time!",
+  "Whale, whale, whale...look who's here!",
+  "Gee...this is over-whalming!",
+  "Everything whale be alright!",
+  "Get whale soon!",
+  "Whale done! This looks great!",
+  "What do you call a pod of noisy whales?        An ORCASTRA!"
+)
